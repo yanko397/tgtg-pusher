f7186ca2ecfc72005b4ef0fe073b26a6c5a61caf
Johannes Bräuer <yanko397@web.de>
2023-07-30 17:07:00 +0200

    localization to germany

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
+import pytz
 
 def parse_time(date_time, no_day=False):
-    date_time = datetime.fromisoformat(date_time).astimezone()
+    date_time = datetime.fromisoformat(date_time[:-1])
+    date_time = pytz.utc.localize(date_time).astimezone(pytz.timezone('Europe/Berlin'))
     if date_time.date() == datetime.today().date():
         date_time = date_time.strftime('%H:%M')
     elif date_time.date() == datetime.today().date() + timedelta(days=1):
